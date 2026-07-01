4c467168638a116991872038ec33f9f507f720cf
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-07-01 11:32:20 +0300

    classification calc

--- IntelligencePipeline/Calculators/ClassificationCalculator.cs
+++ IntelligencePipeline/Calculators/ClassificationCalculator.cs
@@ -4,9 +4,23 @@ namespace IntelligencePipeline.Calculators
 {
     class ClassificationCalculator
     {
-        public Classification Calculate(Report report)
+        public void Calculate(Report report)
         {
-            
+            if (report.Priority == Priority.Critical) { report.Classification = Classification.TopSecret; return; }
+            if (report.GetSourceType = "Signal")
+            {
+                string cont = report.Content.ToLower();
+                if (cont.Contains("target") || cont.Contains("attack") || cont.Contains("missle")) { report.Classification = Classification.TopSecret; return; }
+            }
+
+            if (report.Priority == Priority.High) { report.Classification = Classification.Secret; return; }
+            if (report.GetSourceType() == "Signal") { report.Classification = Classification.Secret; return; }
+            if (cont.Contains("weapon") || cont.Contains("border")) { report.Classification = Classification.Secret; return; }
+
+            if (report.Priority == Priority.Medium) { report.Classification = Classification.Restricted; return; }
+            if (report.GetSourceType() == "Soldier") { report.Classification = Classification.Restricted; return; }
+
+            report.Classification = Classification.Unclassified; return;
         }
     }
 }
--- IntelligencePipeline/Calculators/PriorityCalculator.cs
+++ IntelligencePipeline/Calculators/PriorityCalculator.cs
@@ -1,33 +1,31 @@
 using IntelligencePipeline.Models.Reports;
 using IntelligencePipeline.Models.Enums;
-using System.Security.Cryptography.X509Certificates;
 namespace IntelligencePipeline.Calculators
 {
     class PriorityCalculator
     {
-        public Priority Calculate(Report report) 
+        public void Calculate(Report report) 
         {
             string desc = report.Description.ToLower();
-            if (desc.Contains("fire") || desc.Contains("attack") || desc.Contains("missile") || desc.Contains("explosion")) { return Priority.Critical; }
+            if (desc.Contains("fire") || desc.Contains("attack") || desc.Contains("missile") || desc.Contains("explosion")) { report.Priority = Priority.Critical; return; }
             if (report.GetSourceType = "Signal")
             {
                 string cont = report.Content.ToLower(); 
-                if (cont.Contains("target") && cont.Contains("attack")) { return Priority.Critical; }
-                if (cont.Contains("fire") || cont.Contains("attack") || cont.Contains("missile") || cont.Contains("explosion")) { return Priority.Critical; }          
+                if (cont.Contains("target") && cont.Contains("attack")) { report.Priority = Priority.Critical; return; }
+                if (cont.Contains("fire") || cont.Contains("attack") || cont.Contains("missile") || cont.Contains("explosion")) { report.Priority = Priority.Critical; return; }          
             }
-            if (report.GetSourceType() == "Radar" && report.Speed >= 800) { return Priority.Critical; }
+            if (report.GetSourceType() == "Radar" && report.Speed >= 800) { report.Priority = Priority.Critical; return; }
 
-            if (desc.Contains("weapon") || desc.Contains("suspicious") || desc.Contains("border")) { return Priority.High; }
-            if (report.GetSourceType() == "Drone" && report.Altitude > 500) { return Priority.High; }
-            if (report.GetSourceType() == "Radar" && report.Speed >= 400) { return Priority.High; }
-            if (report.GetSourceType() == "Soldier" && report.ConfidenceLevel >= 4 && desc.Contains("movement") { return Priority.High; }
+            if (desc.Contains("weapon") || desc.Contains("suspicious") || desc.Contains("border")) { report.Priority = Priority.High; return; }
+            if (report.GetSourceType() == "Drone" && report.Altitude > 500) { report.Priority = Priority.High; return; }
+            if (report.GetSourceType() == "Radar" && report.Speed >= 400) { report.Priority = Priority.High; return; }
+            if (report.GetSourceType() == "Soldier" && report.ConfidenceLevel >= 4 && desc.Contains("movement") { report.Priority = Priority.High; return; }
 
-            if (desc.Contains("movement") || desc.Contains("vehicle") || desc.Contains("activity")) { return Priority.Medium; }
-            if (report.GetSourceType() == "Radar" && report.Speed >= 120) { return Priority.Medium; }
-            if (report.CalculateReliabilityScore() >= 7) { return Priority.Medium; }
+            if (desc.Contains("movement") || desc.Contains("vehicle") || desc.Contains("activity")) { report.Priority = Priority.Medium; return; }
+            if (report.GetSourceType() == "Radar" && report.Speed >= 120) { report.Priority = Priority.Medium; return; }
+            if (report.CalculateReliabilityScore() >= 7) { report.Priority = Priority.Medium; return; }
 
-            return Priority.Low;
-
+            report.Priority = Priority.Low; return;
         }
     }
 }
